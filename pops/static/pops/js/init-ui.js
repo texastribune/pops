cec21c425e3522bd8b2764a3b64d3475e8223107
crccheck <c@crccheck.com>
2013-02-05 15:09:14 -0600

    attempt to get our styling into the wysiwyg

--- pops/static/pops/js/init-ui.js
+++ pops/static/pops/js/init-ui.js
@@ -4,7 +4,18 @@ $(function(){
 
   $('textarea').each(function(){
     // plugin does not work on collections
-    $(this).wysihtml5();
+    var static_url = '/common/';
+    var options = {
+      "font-styles": false,
+      "html": true,
+      "image": false,
+      "stylesheets": [
+        static_url + "css/reset.css",
+        static_url + "css/texastribune.css",
+        static_url + "css/editorial.css"
+      ]
+    };
+    $(this).wysihtml5(options);
   });
 });
 

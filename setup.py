3e5d57afb109663cede56fa6274c578c785e00e1
Liam Andrew <landrew@texastribune.org>
2015-10-21 13:57:32 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pops',
-    version='0.2.0tt4',
+    version='0.2.0tt5',
     author='Travis Swicegood',
     author_email='travis@domain51.com',
     url='https://github.com/tswicegood/pops',
